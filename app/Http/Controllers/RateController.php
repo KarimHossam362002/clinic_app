03623a36e11af2dd95d847e510a2240241215191
Karim Hossam <140264221+KarimHossam362002@users.noreply.github.com>
2023-09-06 05:52:16 +0300

    rate.index finished

--- app/Http/Controllers/RateController.php
+++ app/Http/Controllers/RateController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Doctor;
-use App\Models\Rate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RateController extends Controller
 {
@@ -13,8 +13,14 @@ class RateController extends Controller
      */
     public function index()
     {
-        $rates = Rate::get();
-        dd($rates);
+        DB::statement("SET SQL_MODE = ''");
+        $rates = DB::table('rates')
+        ->join('doctors','doctors.id','=','rates.doctor_id')
+        ->join('majors','majors.id','=','doctors.major_id')
+        ->select('rates.*','majors.title AS major_title','doctors.name AS doctor_name',DB::raw('SUM(rate) AS sumOfRate'))
+        ->orderByDesc('sumOfRate')
+        ->groupBy('doctor_name')
+        ->get();
         return view('admin.rate.index' ,compact('rates'));
     }
 
